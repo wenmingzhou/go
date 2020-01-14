276d97117abe1190c54375fce1b8cd6a53f7b7cb
524797132 <524797132@qq.com>
2020-01-14 15:33:17 +0800

    web列表，删除

--- src/tech/main.go
+++ src/tech/main.go
@@ -14,6 +14,12 @@ func main() {
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static/"))))
 	http.HandleFunc("/", IndexView)
 	http.HandleFunc("/api/index/data", IndexData)
+
+	http.HandleFunc("/list", ListView)
+	http.HandleFunc("/api/list/data", ListData)
+
+	http.HandleFunc("/api/list/del", ListDel)
+
 	http.ListenAndServe(":8090", nil)
 }
 
@@ -35,6 +41,40 @@ func IndexData(w http.ResponseWriter, r *http.Request) {
 	}
 	buf, _ := json.Marshal(mod)
 	w.Write(buf)
+}
+
+func ListView(w http.ResponseWriter, r *http.Request) {
+	f, _ := os.Open("./views/list.html")
+	io.Copy(w, f)
+	f.Close()
+
+}
+
+func ListData(w http.ResponseWriter, r *http.Request) {
+	mods, err := model.ArticleList()
+	if err != nil {
+		Fail(w, err.Error())
+		return
+	}
+	buf, _ := json.Marshal(mods)
+	w.Write(buf)
+}
+
+func ListDel(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	idstr := r.Form.Get("id")
+	id, _ := strconv.ParseInt(idstr, 10, 64)
+	if model.ArticleDel(id) {
+		Succ(w, "删除成功")
+		return
+	} else {
+		Fail(w, "删除失败")
+		return
+	}
+}
+
+func Succ(w http.ResponseWriter, msg string) {
+	w.Write([]byte(msg))
 }
 
 func Fail(w http.ResponseWriter, msg string) {
--- src/tech/model/article.go
+++ src/tech/model/article.go
@@ -16,3 +16,23 @@ func ArticleGet(id int64) (Article, error) {
 	err := DB.Unsafe().Get(&mod, "select * from Article where id =? limit 1", id)
 	return mod, err
 }
+
+func ArticleList() ([]Article, error) {
+	mods := make([]Article, 0, 10)
+	err := DB.Unsafe().Select(&mods, "select * from Article limit 10")
+	return mods, err
+}
+
+func ArticleDel(id int64) bool {
+	res, _ := DB.Exec("delete from Article where id= ?", id)
+	if res == nil {
+		return false
+	}
+	rows, _ := res.RowsAffected()
+	if rows >= 1 {
+		return true
+	} else {
+		return false
+	}
+
+}
