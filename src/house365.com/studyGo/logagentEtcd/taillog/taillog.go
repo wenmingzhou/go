0c34eb554a1cc007da4ccb500970596e31c81185
524797132 <524797132@qq.com>
2020-04-21 17:13:35 +0800

    etcd 读取多个配置文件 channel 通道发送kafka

--- src/house365.com/studyGo/logagentEtcd/taillog/taillog.go
+++ src/house365.com/studyGo/logagentEtcd/taillog/taillog.go
@@ -1,6 +1,7 @@
 package taillog
 
 import (
+	"context"
 	"fmt"
 	"github.com/hpcloud/tail"
 	"house365.com/studyGo/logagent/kafka"
@@ -13,12 +14,19 @@ type TailTask struct {
 	path     string
 	topic    string
 	instance *tail.Tail
+	//为了能实现退出t.run()
+	ctx        context.Context
+	cancelFunc context.CancelFunc
 }
 
 func NewTailTask(path, topic string) (tailObj *TailTask) {
+	ctx, cancal := context.WithCancel(context.Background())
+
 	tailObj = &TailTask{
-		path:  path,
-		topic: topic,
+		path:       path,
+		topic:      topic,
+		ctx:        ctx,
+		cancelFunc: cancal,
 	}
 	tailObj.init() //根据路径去打开对应的日志
 	return
@@ -37,12 +45,16 @@ func (t *TailTask) init() {
 	if err != nil {
 		fmt.Println("tail file failed,err:", err)
 	}
+	//当goroutine执行的函数退出的时候，goroutine就退出了
 	go t.run() //直接去采集日志发送到kafka
 }
 
 func (t *TailTask) run() {
 	for {
 		select {
+		case <-t.ctx.Done():
+			fmt.Printf("tail task:%s_%s 结束了...\n", t.path, t.topic)
+			return
 		case line := <-t.instance.Lines: //从tailobj的通道中一行一行的读取日志数据
 			//3.2 发往Kafka
 			//kafka.SendToKafka(t.topic,line.Text)  //函数调用函数
