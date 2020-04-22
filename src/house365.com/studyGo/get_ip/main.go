1f5fbb804dff54ed8939a7868f9a885915a64d2c
524797132 <524797132@qq.com>
2020-04-22 13:43:25 +0800

    etcd 读取多个配置文件 channel 通道发送kafka

---
+++ src/house365.com/studyGo/get_ip/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"strings"
+)
+
+func GetOutboundIP() (ip string) {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+	ip = strings.Split(localAddr.IP.String(), ":")[0]
+	return
+}
+
+func main() {
+	ip := GetOutboundIP()
+
+	fmt.Println(ip)
+}
